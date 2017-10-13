83d263822b7d2bc2663d136c093feaba05f044bc
youjingyu <735284268@qq.com>
2017-10-13 15:19:03 +0800

    compelte 'note' style

--- src/modules/note.js
+++ src/modules/note.js
@@ -1,27 +1,49 @@
 const {Composite, TextView, CollectionView} = require('tabris');
 const storage = require('../utils/local-storage');
+const {themeColor} = require('../config');
 
 const note = storage.getNote();
 const collectionViewConfig = {
-    left: 0,
+    left: 10,
     top: 0,
-    right: 0,
+    right: 10,
     bottom: 0,
-    background:'#fff',
+    background: themeColor.backgroundColor,
     itemCount: note.length,
-    cellHeight: 50,
     createCell: () => {
         const textView = new TextView({
-
+            left: 10,
+            right: 10,
+            top: 10,
+            maxLines: 2,
+            font: '20px'
         });
+        const textViewDate = new TextView({
+            left: 10,
+            right: 10,
+            top: 'prev() 5',
+            maxLines: 1,
+            font: '12px'
+        });
+        const hr = new Composite({
+            left: 0,
+            right: 0,
+            top: 'prev() 10',
+            height: 10,
+            background: themeColor.backgroundColor
+        });
+        return new Composite({
+            left: 0,
+            right: 0,
+            background: '#fff'
+        }).append(textView).append(textViewDate).append(hr);
     },
     updateCell: (cell, index) => {
-        let diary = this.items[index];
-        if(diary && !diary.loading && !diary.nothing){
-            createCell(cell, diary);
-        }
+        const textView = cell.find('TextView');
+        textView[0].text = note[index].content;
+        textView[1].text = note[index].date;
     }
-}
+};
 
 module.exports = class Note extends CollectionView{
     constructor(properties){
