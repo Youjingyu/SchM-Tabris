543a862d492f302ec2a9504b1f677ddad5b8611c
youjingyu <735284268@qq.com>
2017-10-13 15:17:21 +0800

    add color configuration and logo image

--- src/config.js
+++ src/config.js
@@ -3,7 +3,10 @@ const debug = true;
 module.exports = {
     apibase: debug ? 'http://192.168.3.26:8082/' : 'http://api.pro.com/',
     themeColor: {
-        mainColor: '#FF6656'
+        mainColor2: '#FF6656',
+        mainColor: '#FB71A2',
+        backgroundColor: '#FFF6F9',
+        logoColor: '#BA103A'
     }
 };
 
